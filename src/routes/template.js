c3d244c09c163520cd452d070281e16fcfc53cae
Joshua Winters-Brown <second.parvenu@outlook.com>
2020-04-14 15:03:22 -0400

    Implimented Patient api

--- src/routes/template.js
+++ src/routes/template.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // Local Constants
 var MongooseClient = require('mongoose');
-const document = require('../schema/departments');
+const document = require('../schema/users');
 
 // Configure our Mongoose Client
 MongooseClient.connect('mongodb://localhost/bad-c', { useNewUrlParser: true, useUnifiedTopology: true });
