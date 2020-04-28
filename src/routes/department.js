6b032853c3d01792763ac7b26d538a4521e7d017
Joshua Winters-Brown <second.parvenu@outlook.com>
2020-04-27 21:54:16 -0400

    Implimented departments

---
+++ src/routes/department.js
@@ -0,0 +1,230 @@
+// Import Librarys
+const express = require("express");
+const path = require("path");
+
+// Local Constants
+var MongooseClient = require("mongoose");
+const document = require("../schema/departments");
+const users = require("../schema/users");
+
+// Configure our Mongoose Client
+MongooseClient.connect("mongodb://localhost/bad-c", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+// Access our global router object.
+let router = express.Router();
+
+router.get("/create", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/login.html");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				// TODO: Create custom error for login.html
+				res.redirect("/login.html");
+			} else {
+				// Check if our user is an admin.
+				if (results.admin == 1) {
+					res.sendFile(
+						path.join(
+							__dirname,
+							"../../private/admin/department/new.html"
+						)
+					);
+				} else {
+					// IF controller return controller dashboard
+					res.redirect("/api/v2/controller/");
+				}
+			}
+		});
+	}
+});
+
+router.post("/create", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/login.html");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				// TODO: Create custom error for login.html
+				res.redirect("/login.html");
+			} else {
+				// Check if our user is an admin.
+				if (results.admin == 1) {
+					const kName = req.body.name;
+
+					var query = {
+						name: kName,
+					};
+
+					document.findOne(query, (err, result) => {
+						// Handle any errors that might occure while reading database.
+						if (err) return console.error(err);
+
+						if (!result) {
+							var new_department = new document(query);
+							new_department.save();
+
+							res.json(new_department);
+						} else {
+							res.redirect("/api/v2/department/update");
+						}
+					});
+				} else {
+					// IF controller return controller dashboard
+					res.redirect("/api/v2/controller/");
+				}
+			}
+		});
+	}
+});
+
+router.get("/read", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/login.html");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				// TODO: Create custom error for login.html
+				res.redirect("/login.html");
+			} else {
+				document.find((err, result) => {
+					res.json(result);
+				});
+			}
+		});
+	}
+});
+
+router.get("/read/:id", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/login.html");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				// TODO: Create custom error for login.html
+				res.redirect("/login.html");
+			} else {
+				document.find({ _id: req.params.id }, (err, result) => {
+					res.json(result);
+				});
+			}
+		});
+	}
+});
+
+router.get("/update", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/login.html");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				// TODO: Create custom error for login.html
+				res.redirect("/login.html");
+			} else {
+				// Check if our user is an admin.
+				if (results.admin == 1) {
+					res.sendFile(
+						path.join(
+							__dirname,
+							"../../private/admin/department/manage.html"
+						)
+					);
+				} else {
+					// IF controller return controller dashboard
+					res.redirect("/api/v2/controller/");
+				}
+			}
+		});
+	}
+});
+
+// Export router contents
+module.exports = router;
