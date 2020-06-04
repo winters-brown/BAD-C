7ca9908bd22f0fbd611886a1c60d648a6d9b4764
Joshua Winters-Brown <second.parvenu@outlook.com>
2020-06-03 20:10:15 -0400

    Implemented CRUD for departments 👍😁😁😁

--- src/routes/department.js
+++ src/routes/department.js
@@ -94,11 +94,9 @@ router.post("/create", (req, res) => {
 							var new_department = new document(query);
 							new_department.save();
 
-							res.render("admin/department/update", {
-								success: "Department created!",
-							});
+							res.redirect("/api/v2/department/create");
 						} else {
-							res.render("admin/department/update", {
+							res.render("admin/department/create", {
 								error: "Department already exists!",
 							});
 						}
@@ -171,9 +169,7 @@ router.get("/read/:id", (req, res) => {
 			if (!results) {
 				res.redirect("/api/v2/auth/login");
 			} else {
-				document.find({ _id: req.params.id }, (err, result) => {
-					res.json(result);
-				});
+				res.redirect("/api/v2/department/delete");
 			}
 		});
 	}
@@ -207,9 +203,19 @@ router.get("/update", (req, res) => {
 				// Check if our user is an admin.
 				if (results.admin == 1) {
 					document.find((err, results) => {
+						var my_departments = new Array();
+
+						for (let step = 0; step < results.length; step++) {
+							var temp = {
+								_id: results[step]._id,
+								name: results[step].name,
+							};
+							my_departments.push(temp);
+						}
+
 						res.render("admin/department/update", {
 							error: err,
-							departments: [results],
+							my_departments,
 						});
 					});
 				} else {
@@ -221,14 +227,138 @@ router.get("/update", (req, res) => {
 	}
 });
 
-router.get("/delete", (req, res) => {
-	// TODO: Complete this.
-	res.send("GET /delete");
+router.get("/update/:id/:name", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/api/v2/auth/login");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				res.redirect("/api/v2/auth/login");
+			} else {
+				// TODO: Search database for the following data and update its content.
+				const temp_id = req.params.id;
+				const temp_name = req.params.name;
+				document.findOneAndUpdate(
+					{ _id: temp_id.toString() },
+					{ name: temp_name.toString() },
+					(err, result) => {
+						res.redirect("/api/v2/department/update");
+					}
+				);
+			}
+		});
+	}
 });
 
-router.post("/delete/:id", (req, res) => {
-	// TODO: Complete this.
-	res.send("GET /delete/:id");
+router.get("/delete", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/api/v2/auth/login");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				res.redirect("/api/v2/auth/login");
+			} else {
+				// Check if our user is an admin.
+				if (results.admin == 1) {
+					document.find((err, results) => {
+						var my_departments = new Array();
+
+						for (let step = 0; step < results.length; step++) {
+							var temp = {
+								_id: results[step]._id,
+								name: results[step].name,
+							};
+							my_departments.push(temp);
+						}
+
+						res.render("admin/department/delete", {
+							error: err,
+							my_departments,
+						});
+					});
+				} else {
+					// IF controller return controller dashboard
+					res.redirect("/api/v2/controller/");
+				}
+			}
+		});
+	}
+});
+
+router.get("/delete/:id", (req, res) => {
+	// Store client cookies locally.
+	const kID = req.cookies.id;
+	const kSession_token = req.cookies.session_token;
+
+	// Build our query for later.
+	var query = {
+		_id: kID,
+		session_token: kSession_token,
+	};
+
+	// Verify query is not null
+	if (query._id == null && query.session_token == null) {
+		// User hasent logged in yet or users session_token is exprired.
+		res.redirect("/api/v2/auth/login");
+	} else {
+		// Search our database with query
+		users.findOne(query, (err, results) => {
+			// Handle any errors that might occure while reading databse.
+			if (err) return console.error(err);
+
+			// No user exists with that id and session_token.
+			if (!results) {
+				res.redirect("/api/v2/auth/login");
+			} else {
+				// Check if our user is an admin.
+				if (results.admin == 1) {
+					var new_query = {
+						_id: req.params.id,
+					};
+					document.deleteOne(new_query, (err2, results2) => {
+						res.redirect("/api/v2/department/delete");
+					});
+				} else {
+					// IF controller return controller dashboard
+					res.redirect("/api/v2/controller/");
+				}
+			}
+		});
+	}
 });
 
 // Export router contents
