6321012d7079350d593fd4c3462de85f0b225b49
Joshua Winters-Brown <second.parvenu@outlook.com>
2020-06-03 20:09:14 -0400

    Implemented hash decrypting test <h1>Department Manager</h1>

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-console.log("I dont want emails because CI/CD failed");
-// TODO: fix this laters
+const bcrypt = require("bcrypt");
+
+data = "test";
+hash = "$2b$10$AhjT1K/27Mmgyesc3UufW.B24mM/ovSs56m9vdnxJ6OwBfS/TEiRa";
+
+bcrypt.compare(data, hash).then(function (result) {
+	console.log(result);
+});
